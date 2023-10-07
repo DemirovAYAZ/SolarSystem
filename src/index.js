56ea4600f75cf889dac277b537db125b03e729ed
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2023-10-07 14:00:31 +0400

    Files Updated

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SolarSystem from './js/SolarSystem';
+ReactDOM.render(
+  <React.StrictMode>
+    <SolarSystem />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
